f344a5ad6124236368252a63d055364d1a5f0d6e
Androprog <43544081+Androprog@users.noreply.github.com>
2018-09-26 22:46:20 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function tur(turler){
+var tur = document.getElementById(turler);
+return tur;
+}
+var aksiyon = tur("aksiyon");
+var macera = tur("macera");
+function goster() {
+var turler = document.getElementById("turler");
+if (turler.value == "Aksiyon"){
+aksiyon.style.display = "block";
+}
+else {
+if (turler.value == "Macera"){
+macera.style.display = "block";
+};
+};
+};
+function kapat() {
+aksiyon.style.display = "none";
+macera.style.display = "none";
+aksiyon.style.display = "none";
+aksiyon.style.display = "none";
+};
