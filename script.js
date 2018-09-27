72c7c73b1a8eadb795c99a21dc86de20eac2e6ac
Androprog <43544081+Androprog@users.noreply.github.com>
2018-09-27 14:09:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ function baslik_degistir(baslik_adi){
 }
 
 function add_to_box(category){
+  var main = document.getElementById("diziler");
+  main.innerHTML = "";
   var series = {"Savaş": [{"title": "Attack", "desc": "asdasdasd"}, {"title": "XFCE", "desc": "asdasdasd"}], "Aksiyon": [{"title": "Naruto", "desc": "asdasdasd"}, {"title": "Narutonun Çocuğu", "desc": "asdasdasd"}]};
-  var main = document.getElementById("box");
   for (x in series[category]) {
     main.innerHTML += formatter(series[category][x]["title"], series[category][x]["desc"]);
   }
