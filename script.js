babeee2dee2bc617446a2b174e23f679d21e8c05
Androprog <43544081+Androprog@users.noreply.github.com>
2018-09-27 13:59:50 +0300

    js güncellendi ["buggy"]

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-var template = `
+function formatter(anime, desc){
+  return `
 	<article>
 		<header>
-			<h3> </h3>
+			<h3>${anime}</h3>
 		</header>
 		<section>
-			<p></p>
+			<p>${desc}</p>
 		</section>
 	</article>
-`
+`    
+}
 
 function baslik_degistir(baslik_adi){
 	var baslik = document.getElementById("titler");
+  add_to_box(baslik_adi);
 	baslik.innerHTML = baslik_adi;
+  console.log(baslik_adi);
 }
-console.log(1)
+
+function add_to_box(category){
+  var series = {"Savaş": [{"title": "Attack"}], "Aksiyon": [{"title": "Naruto"}]};
+  var main = document.getElementByID("box");
+  console.log(series)
+  for (x in series[category]) {
+    console.log(x);
+    main.innerHTML += formatter(x["title"], "");
+  }
+}
+
